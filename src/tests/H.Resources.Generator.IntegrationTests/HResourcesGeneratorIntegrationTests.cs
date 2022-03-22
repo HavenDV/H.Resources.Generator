2bcb8615e4c20df067d59632d446a4954e5bf904
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-03-22 20:53:34 +1000

    test: Added IntegrationTests.

---
+++ src/tests/H.Resources.Generator.IntegrationTests/HResourcesGeneratorIntegrationTests.cs
@@ -0,0 +1,11 @@
+﻿namespace H.Ipc.Generator.IntegrationTests;
+
+[TestClass]
+public class HResourcesGeneratorIntegrationTests
+{
+    [TestMethod]
+    public void GeneratesHelloWorldAsStringCorrectly()
+    {
+        H.Resources.helloworld_txt.AsString().Should().Be("Hello, World!");
+    }
+}
